6c4227b441fa9cbefad7fde337a6e576daacc117
MelentQ <mejleht@yandex.ru>
2022-02-09 15:55:59 +0300

    Страница О компании

--- src/js/custom/initMaps.js
+++ src/js/custom/initMaps.js
@@ -1,14 +1,18 @@
+import * as bodyScrollLock from "body-scroll-lock";
+
 export default function initMaps() {
   const maps = Array.from(document.querySelectorAll('.js-init-map'));
-  if (maps) loadApi('yandex', 'https://api-maps.yandex.ru/2.1/?apikey=99155e03-fba1-47e8-8e51-62786c760fbc&lang=ru_RU',() => {
-    ymaps.ready(init);
-  });
+  if (maps) loadApi('yandex', 'https://api-maps.yandex.ru/2.1/?apikey=99155e03-fba1-47e8-8e51-62786c760fbc&lang=ru_RU',() => {ymaps.ready(init);});
 
   function init() {
     maps.forEach(map => {
+      initMobileMap(map);
+
+      const center = [map.dataset.initialLongitude, map.dataset.initialLatitude];
+
       const yMap = new ymaps.Map(map, {
-        center: [55.874062, 49.220554],
-        zoom: 14,
+        center,
+        zoom: map.dataset.initialZoom,
         controls: []
       });
 
@@ -25,28 +29,53 @@ export default function initMaps() {
           55.874062,
           49.220554
         ],
-        image: "../img/map-marker.svg"
+        image: map.dataset.image
       })
     })
   }
 
   function _addPlace(map, {coords, image}) {
+    var layout = ymaps.templateLayoutFactory.createClass(`
+      <div class="placemark">
+        <img class="placemark__image" src="{{ properties.image }}">
+        <span class="placemark__label">{{ properties.iconCaption }}</span>
+      </div>
+    `);
+
     const placemarkProperties = {
+      image,
       iconCaption: "Россия, город Казань, Ул. Липатова 37, к.9"
     };
 
     const placemarkOptions = {
-        iconLayout: 'default#image',
-        iconImageHref: image,
+        iconLayout: layout,
         iconImageSize: [36, 36],
         iconImageOffset: [-18, -18]
     };
 
     const placemark = new ymaps.Placemark(coords, placemarkProperties, placemarkOptions);
 
-    console.log(placemark);
-
     map.geoObjects.add(placemark);
+  }
+
+  function initMobileMap(map) {
+    const wrapper = map.closest('.js-map-wrapper');
+    console.log(wrapper);
+    const modalWrapper = wrapper.querySelector('.contacts__map-wrapper');
+    const openBtn = wrapper.querySelector('.js-open-map');
+    const closeBtn = wrapper.querySelector('.js-close-map');
+
+    console.log(openBtn);
+
+    openBtn.addEventListener('click', () => {
+      modalWrapper.classList.add('active');
+      bodyScrollLock.disableBodyScroll(modalWrapper);
+    })
+
+    closeBtn.addEventListener('click', () => {
+      modalWrapper.classList.remove('active');
+      bodyScrollLock.enableBodyScroll(modalWrapper);
+    })
   }
 }
 
